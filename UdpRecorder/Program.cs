9eaf1ead4b664b25e2a047be383a2ffda1f8ef64
Peter Hamberg <zazcallabah@gmail.com>
2015-05-16 12:10:39 +0200

    Add helper application that can send testing control data

---
+++ UdpRecorder/Program.cs
@@ -0,0 +1,59 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System;
+
+namespace ConsoleApplication4
+{
+
+	class Program
+	{
+		static bool halt = false;
+		static string[] packets;
+		static void Main( string[] args )
+		{
+			var UdpRecv = new Thread( new ThreadStart( Loop ) );
+			UdpRecv.IsBackground = true;
+			UdpRecv.Start();
+			Console.ReadKey();
+			halt = true;
+			UdpRecv.Join();
+			File.WriteAllLines( "testdata.bin", packets );
+		}
+
+		Thread UdpRecv;
+		UdpClient listener;
+
+		// listen for udp packets in a separate thread.
+		static void Loop()
+		{
+			var l = new List<string>();
+			var listener = new UdpClient( 10001 ); // 10001 is the port, obv. remember to open the firewall!
+			IPEndPoint groupEP = new IPEndPoint( IPAddress.Any, 10001 );
+			try
+			{
+				while( !halt )
+				{
+					byte[] bytes = listener.Receive( ref groupEP );
+					var str = System.Text.Encoding.UTF8.GetString( bytes );
+					l.Add( DateTime.Now.Ticks + "@" + str );
+					Thread.Sleep( 1 );
+				}
+
+			}
+			catch( System.Exception e )
+			{
+			}
+			finally
+			{
+				listener.Close();
+			}
+			packets = l.ToArray();
+		}
+	}
+}
---
+++ UdpSender/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	class DLine
+	{
+		public long TimeStamp { get; set; }
+		public static long Offset = 0;
+		public byte[] Data { get; set; }
+
+		public string tag { get; set; }
+
+		public DLine( string line )
+		{
+			var components = line.Split( '@' );
+			Data = Encoding.UTF8.GetBytes( components[1] );
+			tag = components[1].Substring( 0, Math.Min( 10, components[1].Length ) );
+			TimeStamp = Int64.Parse( components[0] );
+		}
+	}
+	class Program
+	{
+		static int port = 10001;
+		static IPEndPoint endpoint;
+		static void Main( string[] args )
+		{
+			endpoint = new IPEndPoint( new IPAddress( new byte[] { 127, 0, 0, 1 } ), port );
+			string infile = "testdata.bin";
+			var slist = File.ReadAllLines( infile ).Select( l => new DLine( l ) ).ToArray();
+
+
+			using( UdpClient c = new UdpClient( 45232 ) )
+			{
+
+				while( SendList( slist, c ) )
+					Thread.Sleep( 4000 );
+			}
+		}
+
+		static bool SendList( DLine[] list, UdpClient c )
+		{
+			var firststamp = list[0].TimeStamp;
+			var timestarted = DateTime.Now.Ticks;
+			DLine.Offset = timestarted - firststamp;
+
+			foreach( var line in list )
+			{
+				while( true )
+				{
+					if( DateTime.Now.Ticks > line.TimeStamp + DLine.Offset )
+					{
+						c.Send( line.Data, line.Data.Length, endpoint );
+						break;
+					}
+
+					//if( Abort() )
+					//	return false;
+					//else
+					Thread.Yield();
+				}
+			}
+			return true;
+		}
+	}
+}
